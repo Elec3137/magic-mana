3ac1b91cb8f146fed09a5d412bba46f7ad5414c5
electria <elec3137+git@proton.me>
2026-06-29 10:15:20 -0700

    very basic thingy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+  :root {
+    background-color: black;
+    color: white;
+  }
+</style>
+
+<script lang="ts">
+  import { SvelteMap } from "svelte/reactivity";
+
+  let values = new SvelteMap([
+    ["white", 0],
+    ["blue", 0],
+    ["black", 0],
+    ["red", 0],
+    ["green", 100],
+    ["colorless", 0],
+  ]);
+</script>
+
+{#each values as value}
+  <div>
+    <button onclick={() => values.set(value[0], value[1] - 100)}>-100</button>
+    <button onclick={() => values.set(value[0], value[1] - 10)}>-10</button>
+    {value[0]} {value[1]}
+    <button onclick={() => values.set(value[0], value[1] + 10)}>+10</button>
+    <button onclick={() => values.set(value[0], value[1] + 100)}>+100</button>
+  </div>
+{/each}
