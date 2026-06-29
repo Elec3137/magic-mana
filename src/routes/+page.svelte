07f42221fd218281b83a45967bc1779d9d602d61
electria <elec3137+git@proton.me>
2026-06-29 11:11:41 -0700

    add +-50 buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,10 +21,12 @@
 {#each values as value}
   <div>
     <button onclick={() => values.set(value[0], value[1] - 100)}>-100</button>
+    <button onclick={() => values.set(value[0], value[1] - 50)}>-50</button>
     <button onclick={() => values.set(value[0], value[1] - 10)}>-10</button>
     <button onclick={() => values.set(value[0], value[1] - 1)}>-1</button>
     <button onclick={() => values.set(value[0], value[1] + 1)}>+1</button>
     <button onclick={() => values.set(value[0], value[1] + 10)}>+10</button>
+    <button onclick={() => values.set(value[0], value[1] + 50)}>+50</button>
     <button onclick={() => values.set(value[0], value[1] + 100)}>+100</button>
     <img src="/{value[0]}.webp" alt="mana type icon">
     {value[0]} {value[1]}
